da3e184bba27ee9a9af69db60829d1838e20539b
cfay1810 <chelseyfay1810@gmail.com>
2019-04-16 12:57:14 -0500

    Edited pages for correct navigation

--- hackernewsapp/src/App.vue
+++ hackernewsapp/src/App.vue
@@ -2,11 +2,23 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/news">News</router-link>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+  import Navbar from "@/components/Navbar";
+  export default {
+    name: "App",
+    components: {
+      Navbar
+    }
+  };
+</script>
+
+<style></style>
 
 <style lang="scss">
 #app {
