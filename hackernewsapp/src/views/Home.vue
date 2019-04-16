cd34db15adfb0a5e01d6c5c5be9ea0f42cc75d3b
cfay1810 <chelseyfay1810@gmail.com>
2019-04-16 14:51:22 -0500

    Added API Call

--- hackernewsapp/src/views/Home.vue
+++ hackernewsapp/src/views/Home.vue
@@ -1,12 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="My logo" src="../assets/logo.png">
-   <h2>Home Page</h2>
-  </div>
+    <div>
+        <h2>Hacker News</h2>        
+        <div v-for="story in stories" :key="story">
+            <h3>{{ story.data.title }} | Author: {{ story.data.by }}</h3>
+            <p>{{ story.data.url }}</p>
+            </div>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: "home"
-  };
+    import axios from 'axios'
+
+    export default {
+
+        name: 'Homepage',
+        data:  function () {
+            return {
+                err: '',
+                stories: []
+            }
+        },
+        created: function () {
+            // https://hacker-news.firebaseio.com/v0/topstories.json
+            axios.get('https://hacker-news.firebaseio.com/v0/topstories.json')
+                .then((response) => {
+                    let results = response.data.slice(0, 10)
+                    results.forEach(id => {
+                        axios.get('https://hacker-news.firebaseio.com/v0/item/' + id + '.json')
+                            .then((response) => {
+                                this.stories.push(response)
+                            })
+                            .catch((err) => {
+                                this.err = err
+                            })
+                    })
+                })
+                .catch((err) => {
+                    this.err = err
+                })
+        }
+    }
 </script>
+
+<style scoped>
+</style>
